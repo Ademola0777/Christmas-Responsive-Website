316276a0dbeba7cc84edbf5b90ef9790f99c2db0
Khaleed <saludeenkhaleed13gmail.com>
2025-12-16 20:31:54 +0100

    starter

---
+++ assets/script.js
@@ -0,0 +1,29 @@
+// Menu Show and Hide
+
+
+// Close menu when clicking a nav link
+
+
+// Change background header on scroll
+
+
+// active link switching
+
+
+// Dark Light Theme
+
+
+// Restore saved preference
+
+
+// Toggle theme
+
+
+// Swiper
+
+
+// AOS Animation
+
+
+// Show scroll up button
+
